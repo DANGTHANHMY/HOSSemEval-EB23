eb51ad100adc1b998063b6bd4731abb360892482
DANGTHANHMY <dangthanhmyst@gmail.com>
2024-10-08 15:50:42 +0700

    updates

--- TAS/TAS-Transformers/modeling.py
+++ TAS/TAS-Transformers/modeling.py
@@ -34,8 +34,8 @@ class BertForTABSAJoint(nn.Module):
 		self.max_seq_length = max_seq_length
 
 	def forward(self, input_ids, token_type_ids, attention_mask, labels, ner_labels):
-		#outputs = self.bert(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)
-		outputs = self.bert(input_ids, attention_mask=attention_mask)
+		outputs = self.bert(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)
+		#outputs = self.bert(input_ids, attention_mask=attention_mask)
 		all_encoder_layers, pooled_output = self.bert(input_ids, attention_mask)
 		# get the last hidden layer
 		sequence_output = outputs.last_hidden_state
